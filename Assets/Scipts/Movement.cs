bfe721bad87cae9c160a114b0a24096dc23c2931
mrwadepro <mrwadepro@gmail.com>
2022-11-15 22:12:35 -0600

    initial push

---
+++ Assets/Scipts/Movement.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+   [SerializeField] float mainThrust = 100;
+
+   [SerializeField] float rotationSpeed = 100;
+
+Rigidbody rb;
+AudioSource audioData;
+
+
+ //Play the music
+    bool m_Play;
+    //Detect when you use the toggle, ensures music isn’t played multiple times
+    bool m_ToggleChange;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       rb = GetComponent<Rigidbody>();
+         audioData = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessThrust();
+            ProcessRotation();
+    }
+
+    void ProcessThrust(){
+       if(Input.GetKey(KeyCode.Space)){
+        rb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+      if(!audioData.isPlaying)
+        audioData.Play(0);
+        }
+        else{
+          audioData.Stop();
+        }
+       
+ 
+    }
+
+      void ProcessRotation(){
+      if(Input.GetKey(KeyCode.A)){
+
+ApplyRotation(rotationSpeed);
+       }
+
+      else  if(Input.GetKey(KeyCode.D)){
+         ApplyRotation(-rotationSpeed);
+       
+       }
+
+      }
+
+      void ApplyRotation(float rotationThisFrame){
+         rb.freezeRotation = true; // freezing rotation so we can manually rotate
+transform.Rotate(Vector3.forward * rotationThisFrame * Time.deltaTime);
+        rb.freezeRotation = false; // unfreezing rotation so we can manually rotate
+      }
+      
+}
